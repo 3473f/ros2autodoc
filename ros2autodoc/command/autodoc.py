e5c329d579b4072301181039c9bc57d5f379db56
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2023-02-12 20:08:25 +0100

    Initial commit

---
+++ ros2autodoc/command/autodoc.py
@@ -0,0 +1,21 @@
+from ros2cli.command import add_subparsers_on_demand
+from ros2cli.command import CommandExtension
+
+class AutodocCommand(CommandExtension):
+    """Various service related sub-commands."""
+
+    def add_arguments(self, parser, cli_name):
+        self._subparser = parser
+        add_subparsers_on_demand(
+            parser, cli_name, '_verb', 'ros2autodoc.verb', required=False)
+
+    def main(self, *, parser, args):
+        if not hasattr(args, '_verb'):
+            # in case no verb was passed
+            self._subparser.print_help()
+            return 0
+
+        extensions = getattr(args, '_verb')
+
+        # call the verb's main method
+        return extensions.main(args=args)
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'ros2autodoc'
+
+setup(
+  name=package_name,
+  version='0.0.0',
+  packages=find_packages(exclude=['test']),
+  install_requires=['ros2cli'],
+  zip_safe=True,
+  author='Mohamed Abdelaziz',
+  author_email='mohamed.abdelaziz@zal.aero',
+  #url='https://github.com/artivis/ros2hellocli',
+  #download_url='https://github.com/artivis/ros2hellocli/releases',
+  keywords=[],
+  classifiers=[
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: Apache Software License',
+      'Programming Language :: Python',
+  ],
+  description='CLI command to automatically generate documentation for ROS2 nodes in markdown syntax.',
+  long_description="""The project provides a CLI command to automatically generate documentation for ROS2 nodes in markdown syntax.""",
+  license='Apache License, Version 2.0',
+  tests_require=['pytest'],
+  entry_points={
+        'ros2cli.command': [
+            'autodoc = ros2autodoc.command.autodoc:AutodocCommand',
+        ]
+    }
+)

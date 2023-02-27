d0026bb1aea5cadfb710ef60bff55ff87809805f
Johann Christensen <johannchristensen@outlook.de>
2023-02-27 10:01:20 +0000

    Remove stupid linters
    flake8: installs some plugins that are not pep8 compliant, such as a
    plugin to enforce single quotes. Our formatter (black) solely uses
    double quotes and for good reason.
    pep257: straight-up doesn't work, is also handled by our pre-commits
    pep8: no longer available

--- ros2autodoc/api/__init__.py
+++ ros2autodoc/api/__init__.py
@@ -1,3 +1,32 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2023, Mohamed Abdelaziz
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 import sys
 
@@ -32,7 +61,7 @@ def check_for_package(package_name):
 
 
 def check_for_node(node, node_name):
-    """check if the given node is available and running."""
+    """Check if the given node is available and running."""
     running_nodes = get_node_names(node=node, include_hidden_nodes=True)
     count = [n.full_name for n in running_nodes].count(node_name)
     if count > 1:

3eddb1fb88baf955d7339566277c53a8088ea500
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2023-09-11 00:20:06 +0200

    evaluate variable correcrly

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -5,18 +5,20 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/colcon_ws/install/setup.bash"
 
-package=${package:-""}
-node=${node:-""}
+# debugging
+echo "Package is: $package"
+echo "Node is: $node"
 
 # run ros2autodoc
-if [[ -z "$package" ]]; then
-    if [[ -z "$node" ]]; then
+if [[ -z $package ]]; then
+    if [[ -z $node ]]; then
         echo "No nodes are provided. Exiting container."
     else
         ros2 autodoc generate $node
     fi
-elif [[ -z "$node" ]]; then
+elif [[ -z $node ]]; then
     echo "No nodes are provided. Exiting container."
 else
     ros2 autodoc generate $node --package-name $package
+    echo "Documentation generated successfully!"
 fi

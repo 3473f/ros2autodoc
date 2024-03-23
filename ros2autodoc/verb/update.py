4db2baef87ff51939c4578aad50e04406ac2abdd
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2024-03-23 22:20:40 +0000

    update parameters

--- ros2autodoc/verb/update.py
+++ ros2autodoc/verb/update.py
@@ -3,7 +3,7 @@ from os.path import isfile
 from ros2cli.node.strategy import NodeStrategy
 from ros2cli.verb import VerbExtension
 
-from ros2autodoc.api import check_for_node
+from ros2autodoc.api import check_for_node, update_documentation
 
 
 class UpdateVerb(VerbExtension):
@@ -18,21 +18,11 @@ class UpdateVerb(VerbExtension):
             "all running nodes from the package will be documented.",
         )
         parser.add_argument(
-            "input-file",
-            metavar="",
-            nargs=1,
+            "input_file",
             help="absolute path of the README.md file to be updated.",
-            required=True,
         )
 
     def main(self, *, args):
-        """
-        This should:
-        1. Parse the current file and store it in a data structure
-        2. Read the current node interface and store it in a similar data structure
-        3. Compare and add/remove accordingly
-        4. Write the output to the README file
-        """
         if not isfile(args.input_file):
             return f"File: '{args.input_file}' could not be found."
 
@@ -41,3 +31,4 @@ class UpdateVerb(VerbExtension):
                 if not check_for_node(node, f"/{node_name}"):
                     print(f"Node '{node_name}' is not running and will be ignored.")
                     continue
+                update_documentation(node, args.input_file)

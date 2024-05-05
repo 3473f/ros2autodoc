a80b2e9cebcda4d373b49ae3f89e7a0c5b21cd1c
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2024-05-05 17:58:19 +0000

    Remove unused Node class

--- ros2autodoc/api/node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self):
-        self.name = None
-        self.parameters = []
-        self.subscribers = []
-        self.publishers = []
-        self.services = []
-        self.actions = []

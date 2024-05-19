50bf445bc4a1b789cdae45da106b4d822b0b41fc
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2024-05-19 22:00:45 +0000

    Implement node runner

---
+++ ros2autodoc/api/node_runner.py
@@ -0,0 +1,23 @@
+from ros2pkg.api import PackageNotFound
+
+from ros2run.api import get_executable_path, run_executable, MultipleExecutables
+
+class NodeRunner:
+    def __init__(self):
+        pass
+
+    def run(self, package_name, executable_name):
+        try:
+            path = get_executable_path(
+                package_name=package_name,
+                executable_name=executable_name)
+        except PackageNotFound:
+            raise RuntimeError(f"Package '{package_name}' not found")
+        except MultipleExecutables as e:
+            msg = 'Multiple executables found:'
+            for p in e.paths:
+                msg += f'\n- {p}'
+            raise RuntimeError(msg)
+        if path is None:
+            return 'No executable found'
+        return run_executable(path=path, argv=[])

03c4ac1196d9912508ea84853422f971aa096346
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2024-05-21 05:23:15 +0000

    Implement interface for launch files

--- ros2autodoc/api/node_runner.py
+++ ros2autodoc/api/node_runner.py
@@ -11,26 +11,31 @@ class NodeRunner:
     def __init__(self):
         self.process = None
 
-    def start(self, package_name, executable_name):
-        try:
-            path = get_executable_path(
-                package_name=package_name, executable_name=executable_name
-            )
-        except PackageNotFound:
-            raise RuntimeError(f"Package '{package_name}' not found")
-        except MultipleExecutables as e:
-            msg = "Multiple executables found:"
-            for p in e.paths:
-                msg += f"\n- {p}"
-            raise RuntimeError(msg)
-        if path is None:
-            return "No executable found"
+    def start(self, package_name, executable_name=None, launch_file=None):
+        """Start a node either using ros2 run or ros2 launch."""
+        if executable_name:
+            try:
+                path = get_executable_path(
+                    package_name=package_name, executable_name=executable_name
+                )
+            except PackageNotFound:
+                raise RuntimeError(f"Package '{package_name}' not found")
+            except MultipleExecutables as e:
+                msg = "Multiple executables found:"
+                for p in e.paths:
+                    msg += f"\n- {p}"
+                raise RuntimeError(msg)
+            if path is None:
+                return "No executable found"
 
-        cmd = [path]
+            cmd = [path]
 
-        # on Windows Python scripts are invocable through the interpreter
-        if os.name == "nt" and path.endswith(".py"):
-            cmd.insert(0, sys.executable)
+            # on Windows Python scripts are invocable through the interpreter
+            if os.name == "nt" and path.endswith(".py"):
+                cmd.insert(0, sys.executable)
+        
+        elif launch_file:
+            cmd = ["ros2", "launch", package_name, launch_file]
 
         self.process = subprocess.Popen(
             cmd, shell=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
--- ros2autodoc/verb/generate.py
+++ ros2autodoc/verb/generate.py
@@ -30,6 +30,12 @@ class GenerateVerb(VerbExtension):
             help="name of the executables for the nodes to be documented.",
         )
         parser.add_argument(
+            "--launch-file",
+            default=None,
+            metavar="launch_file",
+            help="name of the launch file to start the nodes.",
+        )
+        parser.add_argument(
             "--output-dir",
             default=abspath(curdir),
             metavar="",
@@ -50,25 +56,26 @@ class GenerateVerb(VerbExtension):
             return f"Package '{args.package_name}' could not be found."
 
         # Check if all inputs were provided
-        if not args.nodes or not args.executables:
-            return "At least one node and one executable are required."
+        if not args.nodes or not args.executables or not args.launch_file:
+            return "At least one node and one executable or launch file are required."
 
-        # Check if the number of nodes matches the executables
-        if len(args.nodes) != len(args.executables):
-            return_str = (
-                f"Number of nodes ({len(args.nodes)}) "
-                + f"does not match the number of executables ({len(args.executables)})."
-            )
-            return return_str
+        if args.executables:
+            # Check if the number of nodes matches the executables
+            if len(args.nodes) != len(args.executables):
+                return_str = (
+                    f"Number of nodes ({len(args.nodes)}) "
+                    + f"does not match the number of executables ({len(args.executables)})."
+                )
+                return return_str
 
-        # Check if the package contains all the executables
-        paths = get_executable_paths(package_name=args.package_name)
-        executables = [basename(path) for path in paths]
-        missing_executables = [
-            exe for exe in args.executables if exe not in executables
-        ]
-        if missing_executables:
-            return f"Missing executables: {', '.join(missing_executables)}"
+            # Check if the package contains all the executables
+            paths = get_executable_paths(package_name=args.package_name)
+            executables = [basename(path) for path in paths]
+            missing_executables = [
+                exe for exe in args.executables if exe not in executables
+            ]
+            if missing_executables:
+                return f"Missing executables: {', '.join(missing_executables)}"
 
         # Ensure no trailing slash
         args.output_dir = args.output_dir.rstrip("/")
@@ -77,22 +84,45 @@ class GenerateVerb(VerbExtension):
         runner = NodeRunner()
 
         with NodeStrategy(args) as node:
-            for node_name, executable_name in zip(args.nodes, args.executables):
+            if args.launch_file:
+                runner.start(args.package_name, launch_file=args.launch_file)
 
-                runner.start(args.package_name, executable_name)
+                for node_name in args.nodes:
 
-                if not check_for_node(node, f"/{node_name}"):
-                    print(f"Node '{node_name}' is not running and will be ignored.")
-                    continue
-                if args.seperate_files:
-                    document_node(
-                        node, None, node_name, f"{args.output_dir}/{node_name}.md"
-                    )
-                else:
-                    document_node(
-                        node,
-                        args.package_name,
-                        node_name,
-                        f"{args.output_dir}/README.md",
-                    )
+                    if not check_for_node(node, f"/{node_name}"):
+                        print(f"Node '{node_name}' is not running and will be ignored.")
+                        continue
+                    if args.seperate_files:
+                        document_node(
+                            node, None, node_name, f"{args.output_dir}/{node_name}.md"
+                        )
+                    else:
+                        document_node(
+                            node,
+                            args.package_name,
+                            node_name,
+                            f"{args.output_dir}/README.md",
+                        )
+                       
                 runner.stop()
+            
+            elif args.executables:
+                for node_name, executable_name in zip(args.nodes, args.executables):
+
+                    runner.start(args.package_name, executable_name=executable_name)
+
+                    if not check_for_node(node, f"/{node_name}"):
+                        print(f"Node '{node_name}' is not running and will be ignored.")
+                        continue
+                    if args.seperate_files:
+                        document_node(
+                            node, None, node_name, f"{args.output_dir}/{node_name}.md"
+                        )
+                    else:
+                        document_node(
+                            node,
+                            args.package_name,
+                            node_name,
+                            f"{args.output_dir}/README.md",
+                        )
+                    runner.stop()

b513c791695ff13ac4e7fb576f7f49aa7ee9c4fe
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2024-05-21 07:35:08 +0000

    Pump version to 2.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGE_NAME = "ros2autodoc"
 
 setup(
     name=PACKAGE_NAME,
-    version="2.0.0",
+    version="2.1.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/" + PACKAGE_NAME, ["package.xml"]),
